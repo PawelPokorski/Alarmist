146ed3624a57641d46b7bd0094febec9e3909060
Paweł <pawel@MacBook-Air.local>
2025-03-10 11:17:00 +0100

    Add CQRS structure with MediatR. Add Account commands and queries with base interfaces

---
+++ src/Alarmist.API/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+using Alarmist.API.Models;
+using Alarmist.Application.Account.Commands.AddUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alarmist.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController(IMediator mediator) : ControllerBase
+{
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(AddUserCommand command)
+    {
+        var result = await mediator.Send(command);
+
+        if (result.IsSuccess)
+        {
+            return Ok();
+        }
+
+        return BadRequest(result.Errors);
+    }
+}
